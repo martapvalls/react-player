fe385e3f9a2a6e37de3b86861a98c833f7e22736
Marta Peña <marpeva14@hotmail.com>
2021-01-14 12:11:28 +0100

    add react router dom and Login compo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Fragment } from 'react'
-import Header from './components/Header'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+//components
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 
 function App() {
+    let token
     return (
-        <Fragment>
-            <Header />
-            <h1>Hello from main</h1>
-        </Fragment>
+        <Router>
+              <Header />
+
+              <Switch>
+                  <Route exact path="/">
+                      {!token && <Redirect to="/login" />}
+                  </Route>
+                  <Route exact path="/login" component={Login}/>
+                
+                  
+
+              </Switch>
+        </Router>
     );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+    return (  
+        <h1>Hello from login</h1>
+    );
+}
+ 
+export default Login;
