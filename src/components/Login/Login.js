5cb76783a392254fa3c90a5b2a5635e4efda0c97
Marta Peña <marpeva14@hotmail.com>
2021-01-14 12:43:22 +0100

    login compo design and css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (  
-        <h1>Hello from login</h1>
+        <div className="container">
+            <div className="container__form">
+                <h1 className="form__title">Login</h1>
+
+                <form>
+                    <div className="form__fields">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your E-mail"
+                        />
+                    </div>
+                    <div className="form__fields">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
  
